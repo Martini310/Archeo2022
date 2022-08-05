5b6585c7406eb86c085b4f59153ee5e221cf0206
Martin <martin310@wp.pl>
2022-08-05 23:36:02 +0200

    Optimalized func ‘zastosuj_pobranie()’, Added counting rows in func ‘zastosuj_zwrot()’, Added ‘Pokaz wszystko’ button to show all records from db (for tests), Added ‘clear’ buton to remove all rows in treeview OR remove only selected row, Added image to entry, Added checkbox to input another date, Changed window size

--- main.py
+++ main.py
@@ -2,27 +2,34 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter.messagebox import showerror, askyesno
 import sqlite3
-
 with sqlite3.connect("pojazdy.db") as db:
     cursor = db.cursor()
 from datetime import datetime
 import re
 
-# TODO sprawdzenie czy podane dane nie widnieją już w bazie po klinięciu zastosuj
+# TODO sprawdzenie czy podane dane nie widnieją już w bazie po klinięciu zastosuj_pobranie()
+# TODO Potwierdzenie wprowadzenia danych do bazy
+# TODO Możliwość wprowadzenia innej daty
+# TODO Dodać zdjęcie TR do zwrotu, i ładnie to umiejscowić
+# TODO Uprządkować kod i porobić opisy
 
-cursor.execute(""" CREATE TABLE IF NOT EXISTS pojazdy(
-id integer PRIMARY KEY AUTOINCREMENT, 
-nr_rej text NOT NULL, 
-data_pobrania text NOT NULL,
-osoba_pobranie text NOT NULL, 
-operator_pobranie text NOT NULL, 
-data_zwrotu text, 
-osoba_zwrot text, 
-operator_zwrot text); """)
+cursor.execute(""" CREATE TABLE IF NOT EXISTS pojazdy( id integer PRIMARY KEY AUTOINCREMENT, 
+                                                        nr_rej text NOT NULL, 
+                                                        data_pobrania text NOT NULL, 
+                                                        osoba_pobranie text NOT NULL, 
+                                                        operator_pobranie text NOT NULL, 
+                                                        data_zwrotu text, 
+                                                        osoba_zwrot text, 
+                                                        operator_zwrot text); """)
 
 root = tk.Tk()
 root.title("Archeo 2022")
 root.attributes("-topmost", 1)
+
+
+def insert_pobranie_to_db(nrrej, data, osoba, operator):
+    return f""" INSERT INTO pojazdy(nr_rej, data_pobrania, osoba_pobranie, operator_pobranie) 
+        VALUES("{nrrej}", "{data}", "{osoba}", "{operator}"); """
 
 
 def zastosuj_pobranie():
@@ -31,28 +38,24 @@ def zastosuj_pobranie():
     osoba = combobox_pobierajacy.get().title()
     operator = combobox_operator.get().title()
 
-    # TODO do optymalizacji
-
     if teczka == "" or osoba == "":
-        showerror("Błąd", "Pola  'Numer TR' i 'Osoba pobierająca' są obowiązkowe!")
+        # Jeśli nie wpisze się TR lub pobierającego wyskoczy błąd
+        showerror("Błąd", "Pola  'Numer TR' i 'Pobierający akta' są obowiązkowe!")
+
     elif check_tr(teczka):
-        cursor.execute(f""" INSERT INTO pojazdy(nr_rej, data_pobrania, osoba_pobranie, operator_pobranie) 
-        VALUES("{teczka}", "{now}", "{osoba}", "{operator}"); """)
+        # Jeśli nr TR jest poprawny - wstaw dane do bazy
+        cursor.execute(insert_pobranie_to_db(teczka, now, osoba, operator))
         db.commit()
 
-        for n in cursor.execute(""" SELECT * FROM pojazdy"""):
-            archeo_data.insert("", tk.END, values=n)
     elif not check_tr(teczka):
+        # Jeśli nr TR jest błędny pokaż zapytanie
         poprawna_tr = askyesno("Błędny numer rejestracyjny",
                                f"Numer TR powinien składać się z wyróżnika powiatu, ODSTĘPU i pojemnoci.\n"
                                f"Czy '{teczka}' to na pewno poprawny numer rejestracyjny?")
         if poprawna_tr:
-            cursor.execute(f""" INSERT INTO pojazdy(nr_rej, data_pobrania, osoba_pobranie, operator_pobranie) 
-            VALUES("{teczka}", "{now}", "{osoba}", "{operator}"); """)
+            # Po zatwierdzeniu wprowadzi dane do bazy
+            cursor.execute(insert_pobranie_to_db(teczka, now, osoba, operator))
             db.commit()
-
-            for n in cursor.execute(""" SELECT * FROM pojazdy"""):
-                archeo_data.insert("", tk.END, values=n)
 
 
 def zastosuj_zwrot():
@@ -61,9 +64,13 @@ def zastosuj_zwrot():
     osoba = combobox_zwracajacy.get().title()
     operator = combobox_operator.get().title()
 
-    cursor.execute(f""" UPDATE pojazdy SET data_zwrotu = "{now}", osoba_zwrot = "{osoba}", 
+    cursor.execute(f""" SELECT * FROM pojazdy WHERE data_zwrotu IS NOT NULL AND nr_rej = "{teczka}"; """)
+    if len(cursor.fetchall()) > 0:
+        showerror("Błąd", f"Teczka o numerze '{teczka}' została już zwrócona")
+    else:
+        cursor.execute(f""" UPDATE pojazdy SET data_zwrotu = "{now}", osoba_zwrot = "{osoba}", 
     operator_zwrot = "{operator}" WHERE nr_rej = "{teczka}"; """)
-    db.commit()
+        db.commit()
 
     for n in cursor.execute(f""" SELECT * FROM pojazdy WHERE nr_rej = "{teczka}"; """):
         archeo_data.insert("", tk.END, values=n)
@@ -83,22 +90,45 @@ def enable_frame(event):
     zwrot_entry.config(state="enabled")
     combobox_zwracajacy.config(state="enabled")
 
+
+def show_all():
+    for n in cursor.execute(""" SELECT * FROM pojazdy """):
+        archeo_data.insert("", tk.END, values=n)
+
+
+def clear():
+    if archeo_data.selection():
+        selected_data = archeo_data.selection()
+        archeo_data.delete(selected_data)
+    else:
+        archeo_data.delete(*archeo_data.get_children())
+    '''Usuwanie pojedyńczych wpisów
+    sel = archeo_data.selection()
+    archeo_data.delete(sel)'''
+
+def inna_data():
+    if inna_data_pobrania.get():
+        data_pobrania_entry.config(state="enable")
+    if not inna_data_pobrania.get():
+        data_pobrania_entry.config(state="disabled")
+
+
 # WINDOW SIZE & LOCATION
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 
-window_width = int(screen_width * 0.8)
-window_height = int(screen_height * 0.80)
+window_width = 1400
+window_height = 700
 
 center_x = int(screen_width / 2 - window_width / 2)
 center_y = int(screen_height / 2 - window_height / 1.8)
 
 root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
-root.resizable(True, True)
+root.resizable(False, False)
 
 # NOTEBOOK WIDGET
 notebook = ttk.Notebook(root)
-notebook.pack(pady=10, expand=True)
+notebook.pack(pady=10, expand=True, fill="both")
 
 pojazd = ttk.Frame(notebook)
 kierowca = ttk.Frame(notebook)
@@ -161,6 +191,7 @@ combobox_operator.bind("<<ComboboxSelected>>", enable_frame, add="+")
 # CENTER FRAMES
 
 left_frame = ttk.LabelFrame(pojazd, text="Pobranie akt")
+
 left_frame.columnconfigure(0, minsize=100)
 left_frame.columnconfigure(1, weight=1)
 left_frame.columnconfigure(2, weight=1)
@@ -169,6 +200,7 @@ left_frame.columnconfigure(3, minsize=100)
 separator_frame = ttk.Frame(pojazd)
 
 right_frame = ttk.LabelFrame(pojazd, text="Zwrot akt")
+
 right_frame.columnconfigure(0, minsize=100)
 right_frame.columnconfigure(1, weight=1)
 right_frame.columnconfigure(2, weight=1)
@@ -187,29 +219,41 @@ vertical_separator = ttk.Separator(
 
 vertical_separator.grid(pady=10, ipady=100)
 
-# LEFT FRAME
+# @@@@@@@@@@@@@@ LEFT FRAME @@@@@@@@@@@@@@@@
+tablica = tk.PhotoImage(file="tablica.gif")
+tablica_label = ttk.Label(left_frame, image=tablica)
+tablica_label.place(relx=0.46, rely=0.12)
+
+    # NUMER REJESTRACYJNY
 pobranie_label = ttk.Label(left_frame, text="Numer TR:", background="yellow")
+pobranie_entry = ttk.Entry(left_frame, state="disabled", width=12, justify="center", font="Arial 13 bold")
 
-pobranie_entry = ttk.Entry(left_frame, state="disabled")
-
+    # OSOBA POBIERAJĄCA
 pobierajacy_label = ttk.Label(left_frame, text="Pobierający akta:")
-
 combobox_pobierajacy = ttk.Combobox(left_frame, state="disabled")
-
 combobox_pobierajacy["values"] = ["Błażej Prajs",
                                   "Marzena Ciszek",
                                   "Wojciech Kaczmarek",
                                   ]
 
+    # INNA DATA
+inna_data_pobrania = tk.BooleanVar()
+data_pobrania_check = ttk.Checkbutton(left_frame, onvalue=True, offvalue=False, text="Inna data",
+                                      variable=inna_data_pobrania, command=inna_data)
+data_pobrania_entry = ttk.Entry(left_frame, state="disabled")
+
+    # ZASTOSUJ - PRZYCISK
 zastosuj_pobranie_button = ttk.Button(left_frame, text="Zastosuj", command=zastosuj_pobranie)
 
-pobranie_label.grid(column=1, row=0, sticky="E", pady=30)
-pobranie_entry.grid(column=2, row=0, sticky="WE")
-pobierajacy_label.grid(column=1, row=1, sticky="E", pady=20)
+pobranie_label.grid(column=1, row=0, sticky="E", pady=20, padx=30)
+pobranie_entry.grid(column=2, row=0, sticky="W",)
+pobierajacy_label.grid(column=1, row=1, sticky="E", pady=10)
 combobox_pobierajacy.grid(column=2, row=1, sticky="WE")
-zastosuj_pobranie_button.grid(column=1, columnspan=2, row=2, sticky="WE", pady=5)
+data_pobrania_check.grid(column=1, row=2, sticky="E", pady=10)
+data_pobrania_entry.grid(column=2, row=2, sticky="W")
+zastosuj_pobranie_button.grid(column=1, columnspan=2, row=3, sticky="WE", pady=5)
 
-# RIGHT FRAME
+# @@@@@@@@@@@@@ RIGHT FRAME @@@@@@@@@@@@@@@@
 zwrot_label = ttk.Label(right_frame, text="Numer TR", background="pink")
 
 zwracana_teczka = tk.StringVar()
@@ -246,14 +290,14 @@ horizontal_separator.grid(column=1,
                           pady=15,
                           )
 
-# DATABASE AREA
+# @@@@@@@@@@@@@ DATABASE AREA @@@@@@@@@@@@@@@@@@@
 columns = ("id", "TR", "Data pobrania", "Pobierający", "Operator pobranie",
            "Data zwrotu", "Zwracający", "Operator zwrot")
 
-archeo_data = ttk.Treeview(pojazd, columns=columns, show='headings', displaycolumns='#all')
+archeo_data = ttk.Treeview(pojazd, columns=columns, show='headings', displaycolumns='#all', height=10)
 
 for column in columns:
-    archeo_data.heading(column, text=column)
+    archeo_data.heading(column, text=column, anchor='center')
 
 archeo_data.column("id", width=60)
 archeo_data.column("TR", width=120)
@@ -271,5 +315,13 @@ scrollbar.grid(column=4, row=10, sticky="NS")
 # CLOSE BUTTON
 zamknij_button = ttk.Button(pojazd, text="Zamknij", command=lambda: root.quit())
 zamknij_button.grid(column=2, row=12, sticky="WE", padx=10, pady=10)
+
+# SHOW ALL BUTTON
+show_all_button = ttk.Button(pojazd, text="Pokaż wszystko", command=show_all)
+show_all_button.grid(column=3, row=12, sticky="E")
+
+# CLEAR BUTTON
+clear_button = ttk.Button(pojazd, text="Clear", command=clear)
+clear_button.grid(column=1, row=12, sticky="W")
 
 root.mainloop()
